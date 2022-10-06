e5e60adb532774021983dc51eff0651496fdf99a
shenxuxiang <shen_xuxiang@163.com>
2022-10-06 09:59:13 +0800

    feat: 删除了 setupproxy js文件

--- setupProxy.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-const app = express();
-
-app.use('/api', createProxyMiddleware({ target: 'https://localhost', changeOrigin: true, secure: false }));
-app.listen(9999);
