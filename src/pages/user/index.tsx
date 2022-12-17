51e34836930d0a7dadcc0456cfc0143ac8d7bff6
shenxuxiang <shen_xuxiang@163.com>
2022-12-17 12:46:41 +0800

    build: 生产构建

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -8,7 +8,7 @@ export default class extends PureComponent<any, any> {
         <Space>
           <Button type="primary">用户中心</Button>
           <Button>用户中心</Button>
-          <Button>environment production jenkins</Button>
+          <Button>environment production hhss</Button>
         </Space>
       </section>
     );
