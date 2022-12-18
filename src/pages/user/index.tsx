a958b95e91c20d0b0f35fd4b4cb99172795d6aad
shenxuxiang <shen_xuxiang@163.com>
2022-12-18 16:19:53 +0800

    build: master 构建1111

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -7,7 +7,7 @@ export default class extends PureComponent<any, any> {
       <section>
         <Space>
           <Button type="primary">用户中心</Button>
-          {/* <Button type="primary">production</Button> */}
+          <Button type="primary">production</Button>
           <Button type="dashed">production</Button>
           <Button>用户中心</Button>
         </Space>
