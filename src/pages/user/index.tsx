33a0b1dcd4daacfdd66e3dbbf8fccd77c850fe80
shenxuxiang <shen_xuxiang@163.com>
2022-12-18 16:02:43 +0800

    build: goujin

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -7,7 +7,7 @@ export default class extends PureComponent<any, any> {
       <section>
         <Space>
           <Button type="primary">用户中心</Button>
-          {/* <Button>用户中心</Button> */}
+          <Button>用户中心</Button>
           <Button type="primary">development</Button>
           <Button type="dashed">development</Button>
           <Button>用户中心aa</Button>
