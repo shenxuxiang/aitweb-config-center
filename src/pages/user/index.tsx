0eaaecf636f8c107b01b68fe64fad1dba2fd322b
shenxuxiang <shenxuxiang@ahqianmo.com>
2022-12-22 16:49:38 +0800

    build: dev 分支构建

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -7,10 +7,8 @@ export default class extends PureComponent<any, any> {
       <section>
         <Space>
           <Button type="primary">用户中心</Button>
-          <Button>用户中心</Button>
-          <Button type="primary">development</Button>
-          <Button type="dashed">development</Button>
-          <Button>用户中心aa</Button>
+          <Button type="ghost">用户中心</Button>
+          <Button type="dashed">用户中心</Button>
         </Space>
       </section>
     );
